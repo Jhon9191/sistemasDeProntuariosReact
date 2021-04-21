73ff9e0ef1abef749a85a9c20fc6d686d1a0341e
Jhon9191 <joaoantonio15161516@gmail.com>
2021-04-20 23:19:03 -0300

    Configuring the initial routes

---
+++ src/pages/Signim/index.jsx
@@ -0,0 +1,11 @@
+
+
+const Signim = () => {
+    return (
+        <div>
+            <h1>Signim</h1>
+        </div>
+    );
+}
+
+export default Signim;
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import { Switch } from  'react-router-dom';
+import Route from './Route.jsx'
+
+import SignIn from '../pages/Signim/index.jsx';
+import SignUp from '../pages/Signup/index.jsx';
+import Dashboard from '../pages/Dashboard/index.jsx';
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={SignIn} />
+            <Route exact path="/cadastrar" component={SignUp} />
+            <Route exact path="/dashboard" component={Dashboard} isPrivate/>
+        </Switch>
+    );
+}
+
+export default Routes;
