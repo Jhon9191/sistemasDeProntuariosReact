eeaff4631917f8fd83239df1af53fbdd75e27c74
Jhon9191 <joaoantonio15161516@gmail.com>
2021-05-05 18:38:06 -0300

    Developing layout based on the login screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-
+import './global.css'
 import Routes from './routes';
 
 function App  () {
--- src/pages/Signim/index.jsx
+++ src/pages/Signim/index.jsx
@@ -1,9 +1,24 @@
+import React,{useState} from 'react';
+import styles from './styles.css';
 
-
+import { Link } from 'react-router-dom'
 const Signim = () => {
+
+    const [cpf, setCpf] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
-        <div>
-            <h1>Signim</h1>
+        <div className="container">
+           <div className="login">
+                    <form>
+                        <h1>Entrar</h1>
+                        <input onChange={(e)=>setCpf(e.target.value)} type="text" placeholder="CPF:" />
+                        <input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="SENHA:" />
+                        <button type="submit">Login</button>
+                    </form>
+
+                    <Link to="/register">Criar uma nova conta</Link>
+            </div>
         </div>
     );
 }
