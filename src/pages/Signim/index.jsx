4abddf0714341d068fcf800d1d0d59eb4dc54182
Jhon9191 <joaoantonio15161516@gmail.com>
2021-05-06 16:55:33 -0300

    Organizing structure

--- src/pages/Signim/index.jsx
+++ src/pages/Signim/index.jsx
@@ -10,15 +10,15 @@ const Signim = () => {
     return (
         <div className="img">
             <div className="opac">
-                <div className="login">
+                <div className="formulario">
                     {/* <h1>Olá</h1> */}
                     <form>
                         <h2>Entrar</h2>
                         <input onChange={(e) => setCpf(e.target.value)} type="text" placeholder="CPF:" />
-                        <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="SENHA:" />
+                        <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Senha:" />
                         <button type="submit">Login</button>
                         <Link id="linkto" to="/register">Esqueci minha senha</Link>
-                        <div style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                        <div style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 5 }}>
                             <input className="check" type="checkbox" defaultChecked={() => { }} onChange={() => { }} />
                             <spam id="spamCheck" >Mantenha-me conectado</spam>
                         </div>
