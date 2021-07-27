f682d4070101b2a3f7d352dc79993009c4695127
Jhon9191 <joaoantonio15161516@gmail.com>
2021-07-26 21:06:13 -0300

    Creating the Schedule Page Structure

--- src/pages/Appointment/index.jsx
+++ src/pages/Appointment/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar';
+import { BsReverseLayoutTextWindowReverse } from 'react-icons/bs'
 
+import Header from '../../components/Header'
 import styles from './styles.css';
 
 function Appointment() {
-  return(
-      <div>
-          <Sidebar/>
-      </div>
-  );
+    return (
+        <div>
+            <Sidebar />
+
+            <div className="content">
+                <Header name="Agendar consulta">
+                    <BsReverseLayoutTextWindowReverse size={25} />
+                </Header>
+            </div>
+
+            <div className="content">
+                <div className="bodyContent">
+                    <h1 id="Title">Informe os dados para agendar uma consulta</h1>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default Appointment;
